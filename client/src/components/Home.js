cf452840eb6602504f1b0ba0ea4b23673297510c
DannyDoneva96 <danielasinmas1@gmail.com>
2022-07-28 14:07:31 +0300

    home page view is done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,10 +5,12 @@ export const Home =()=>{
 
     return (<div className="bcgHome"> <section id="home-page" className="home-bcg">
     <div className="center">
-      <h1 className="aboutUs">
+      <h1 className="KD">
         Kiril &amp; Danny
       </h1>
+      <br />
       <h2>Are getting married</h2>
+      <br />
       <h2>
         <b>14.12.2019</b>
       </h2>
