222f0bad445fe0afb8274efb38dd0c5c09bb7c21
DannyDoneva96 <danielasinmas1@gmail.com>
2022-07-29 12:25:26 +0300

    finished about us static template

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,29 +1,49 @@
 
+const pic1 = new URL("/public/images/pic1.jpg" ,import.meta.url)
+const pic2 = new URL("/public/images/pic2.jpg" ,import.meta.url)
+const pic3 = new URL("/public/images/pic3.jpg" ,import.meta.url)
+
 export const About = () => {
     return (
-    <div className="wrapper">
-        <h1>Our Story </h1>
-        <div className="story">
-            <div className="story-1">
-                <h3>Him</h3>
-                <p className="description">lorem ipsum dolor sit amet, consectetur adip non pro id el </p>
-                <p>Some text here Some text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text here</p>
+        <div className="BcgAboutUS">
+            <div className="wrapper">
+                <br />
+                <br />
+                <br />
+                <br />
 
-            </div>
-            <div className="story-1">
-                <h3>Her</h3>
-                <p classNaName="description">lorem ipsum dolor sit amet, consectetur</p>
-                <p>Some text here Some text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text here</p>
+                <h1 className="h1Aboutus">Our Story </h1>
+                <div className="story">
+                    <div className="story-1">
+                        <div className="storyImg">
+                            <img src={pic1} alt="picture" />
+                        </div>
+                        <h3>Him</h3>
+                        <p className="description">lorem ipsum dolor sit amet, consectetur adip non pro id el </p>
+                        <p>Some text here Some text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text here</p>
 
+                    </div>
+                    <div className="story-1">
+                    <div className="storyImg">
+                            <img src={pic2} alt="picture" />
+                        </div>
+                        <h3>Her</h3>
+                        <p className="description">lorem ipsum dolor sit amet, consectetur</p>
+                        <p>Some text here Some text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text here</p>
+
+                    </div>
+                    <div className="story-1">
+                    <div className="storyImg">
+                            <img src={pic3} alt="picture" />
+                        </div>
+                        <h3>Us</h3>
+                        <p className="description"> Some text hereSome text here</p>
+                        <p>Some text here Some text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text here</p>
+                    </div>
+
+
+                </div>
             </div>
-            <div className="story-1">
-                <h3>Us</h3>
-                <p classNaName="description"> Some text hereSome text here</p>
-                <p>Some text here Some text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text hereSome text here</p>
-            </div>
-           
-          
         </div>
-    </div>
     )
 }
