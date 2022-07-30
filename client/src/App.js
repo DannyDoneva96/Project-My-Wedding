579a0c707b3fe03f775b243527dfe47cb70f395e
DannyDoneva96 <danielasinmas1@gmail.com>
2022-07-30 13:04:37 +0300

    not done yet but enought for now

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { Home } from './components/Home'
 import {Footer } from './components/Footer'
 import {Invitation } from './components/Invitation'
 import { About } from './components/About'
-import {Wishes} from './components/Wishes'
 import {Register} from './components/Register'
 import {ErrorPage} from './components/ErrorPage'
 import {RSVP} from './components/RSVP'
@@ -25,7 +24,6 @@ function App() {
         <Route path="/aboutus" element= {<About />} />
         <Route path="/rsvp" element= {<RSVP />} />
 
-        <Route path="/wishingBook" element= {<Wishes />} />
         <Route path="/register" element= {<Register />} />
         <Route path="/error" element= {<ErrorPage />} />
       </Routes>
