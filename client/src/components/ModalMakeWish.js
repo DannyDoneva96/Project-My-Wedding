613332664810626c9009a44ea785581c88b87940
DannyDoneva96 <danielasinmas1@gmail.com>
2022-08-04 11:24:29 +0300

    upgrade the modal add comment

--- client/src/components/ModalMakeWish.js
+++ client/src/components/ModalMakeWish.js
@@ -1,13 +1,37 @@
 export const ModalMakeWish = (props) => {
 
-    if (!props.show){
+    if (!props.show) {
         return null;
     }
     return (
         <div className="modal" onClick={props.onClose}>
-            <div className="modal-content" onClick={e=>e.stopPropagation()}>
+            <div className="modal-content" onClick={e => e.stopPropagation()}>
                 <div className="modal-header">
                     <h4 className="modal-title"> Write Your Wish here! </h4>
+                    <div className="">
+                        <i className="fas fa-user"></i>
+                        <input type="text" placeholder="Name" required></input>
+                        <span className="bar"></span>
+
+                    </div>
+                    <div className="">
+                        <i className="fas fa-user"></i>
+                        <input type="text" placeholder="imgUrl" required></input>
+                        <span className="bar"></span>
+
+                    </div>
+                    <div className="">
+                        <i className="fas fa-user"></i>
+                        <input type="text" placeholder="related" required></input>
+                        <span className="bar"></span>
+
+                    </div>
+                    <div className="">
+                        <i className="fas fa-user"></i>
+                        <input type="text" placeholder="message" required></input>
+                        <span className="bar"></span>
+
+                    </div>
                 </div>
                 <div className="modal-body">This is body</div>
                 <div className="modal-footer"><button type="button" className="modal-button">Send</button>
